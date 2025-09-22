f97784f323a859f8ee53e6113b770d6f0b708bfa
Christian Kodehode <christian404.kodehode@gmail.com>
2025-09-22 21:00:37 +0200

    trying to fix nav bar

--- javascript.js
+++ javascript.js
@@ -1,16 +1,64 @@
+// let lastScrollY = window.scrollY;
+// const navbar = document.getElementById("nav-bar");
+// let ticking = false;
+
+// function handleScroll() {
+//   const currentScrollY = window.scrollY;
+//   const delta = 10;
+
+//   if (Math.abs(currentScrollY - lastScrollY) > delta) {
+//     if (currentScrollY < lastScrollY) {
+//       navbar.classList.remove("hidden");
+//       navbar.classList.add("visible");
+//     } else {
+//       navbar.classList.remove("visible");
+//       navbar.classList.add("hidden");
+//     }
+
+//     lastScrollY = currentScrollY;
+//   }
+
+//   ticking = false;
+// }
+
+// window.addEventListener("scroll", () => {
+//   if (!ticking) {
+//     window.requestAnimationFrame(handleScroll);
+//     ticking = true;
+//   }
+// });
+
 let lastScrollY = window.scrollY;
 const navbar = document.getElementById("nav-bar");
+let ticking = false;
 
-window.addEventListener("scroll", () => {
+function handleScroll() {
   const currentScrollY = window.scrollY;
+  const delta = 10;
 
-  if (currentScrollY < lastScrollY) {
-    navbar.classList.remove("hidden");
-    navbar.classList.add("visible");
-  } else {
-    navbar.classList.remove("visible");
-    navbar.classList.add("hidden");
+  if (Math.abs(currentScrollY - lastScrollY) > delta) {
+    if (currentScrollY < lastScrollY) {
+      // Scrolling up
+      navbar.classList.remove("hidden");
+      navbar.classList.add("visible");
+    } else {
+      // Scrolling down
+      navbar.classList.remove("visible");
+      navbar.classList.add("hidden");
+    }
+
+    lastScrollY = currentScrollY;
   }
 
-  lastScrollY = currentScrollY;
+  ticking = false;
+}
+
+// Ensure navbar is visible on load
+navbar.classList.add("visible");
+
+window.addEventListener("scroll", () => {
+  if (!ticking) {
+    window.requestAnimationFrame(handleScroll);
+    ticking = true;
+  }
 });

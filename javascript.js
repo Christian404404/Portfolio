c37c9b57fa81a8973f9b02ac901719662f4a51fa
Christian Kodehode <christian404.kodehode@gmail.com>
2025-09-22 21:44:48 +0200

    fixed nav bar, it now behaves correctly and looks pretty much how I want it

--- javascript.js
+++ javascript.js
@@ -1,60 +1,26 @@
-// let lastScrollY = window.scrollY;
-// const navbar = document.getElementById("nav-bar");
-// let ticking = false;
-
-// function handleScroll() {
-//   const currentScrollY = window.scrollY;
-//   const delta = 10;
-
-//   if (Math.abs(currentScrollY - lastScrollY) > delta) {
-//     if (currentScrollY < lastScrollY) {
-//       navbar.classList.remove("hidden");
-//       navbar.classList.add("visible");
-//     } else {
-//       navbar.classList.remove("visible");
-//       navbar.classList.add("hidden");
-//     }
-
-//     lastScrollY = currentScrollY;
-//   }
-
-//   ticking = false;
-// }
-
-// window.addEventListener("scroll", () => {
-//   if (!ticking) {
-//     window.requestAnimationFrame(handleScroll);
-//     ticking = true;
-//   }
-// });
-
 let lastScrollY = window.scrollY;
 const navbar = document.getElementById("nav-bar");
 let ticking = false;
+let hideTimeout;
 
 function handleScroll() {
   const currentScrollY = window.scrollY;
-  const delta = 10;
+  const scrollLength = 10;
+  const autoHideDelay = 2000;
 
-  if (Math.abs(currentScrollY - lastScrollY) > delta) {
+  if (Math.abs(currentScrollY - lastScrollY) > scrollLength) {
     if (currentScrollY < lastScrollY) {
-      // Scrolling up
       navbar.classList.remove("hidden");
       navbar.classList.add("visible");
     } else {
-      // Scrolling down
       navbar.classList.remove("visible");
       navbar.classList.add("hidden");
     }
-
     lastScrollY = currentScrollY;
   }
 
   ticking = false;
 }
-
-// Ensure navbar is visible on load
-navbar.classList.add("visible");
 
 window.addEventListener("scroll", () => {
   if (!ticking) {
